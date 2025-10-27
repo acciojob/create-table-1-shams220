21d68609187a2f16ff9df7ed92674961bba6c470
shams alam <103259015+shams220@users.noreply.github.com>
2025-10-27 12:52:15 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,20 @@
 function insert_Row() {
     //Write your code here
+  const btn = document.querySelector('input');
+	btn.addEventListener('click',()=>{
+			const row = document.createElement('tr')
+		const td1  = document.createElement('td')
+		const td2  = document.createElement('td')
+	 td1.textContent ="New Cell1"
+	 td2.textContent ="New Cell2"
+		row.appendChild(td1)
+		row.appendChild(td2)
+		document.querySelector('table').prepend(row)
+		
+		
+		
+})
   
-  
+		
 }
+ 
